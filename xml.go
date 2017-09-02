09041bcba524341350a180727777688719f7dc4c
Todd <todd@baliance.com>
2017-09-02 09:20:19 -0500

    document: fix space preserve on run text content

--- xml.go
+++ xml.go
@@ -9,24 +9,26 @@ package gooxml
 
 import "encoding/xml"
 
+// NeedsSpacePreserve returns true if the string has leading or trailing space.
+func NeedsSpacePreserve(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	switch s[0] {
+	case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0:
+		return true
+	}
+	switch s[len(s)-1] {
+	case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0:
+		return true
+	}
+	return false
+}
+
 // AddPreserveSpaceAttr adds an xml:space="preserve" attribute to a start
 // element if it is required for the string s.
 func AddPreserveSpaceAttr(se *xml.StartElement, s string) {
-	if len(s) == 0 {
-		return
-	}
-	preserveSpace := false
-	switch s[0] {
-	case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0:
-		preserveSpace = true
-	}
-	if !preserveSpace {
-		switch s[len(s)-1] {
-		case '\t', '\n', '\v', '\f', '\r', ' ', 0x85, 0xA0:
-			preserveSpace = true
-		}
-	}
-	if preserveSpace {
+	if NeedsSpacePreserve(s) {
 		se.Attr = append(se.Attr, xml.Attr{Name: xml.Name{Local: "xml:space"}, Value: "preserve"})
 	}
 }
