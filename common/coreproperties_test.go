180c0f958dcbcadac9ede4929a8c52defc38c1be
Todd <todd@baliance.com>
2017-09-01 21:16:35 -0500

    common: flesh out core properties wrapper and add an example

--- common/coreproperties_test.go
+++ common/coreproperties_test.go
@@ -3,10 +3,13 @@ package common_test
 import (
 	"bytes"
 	"encoding/xml"
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"baliance.com/gooxml/common"
+	"baliance.com/gooxml/document"
 	"baliance.com/gooxml/testhelper"
 	"baliance.com/gooxml/zippkg"
 )
@@ -29,4 +32,28 @@ func TestMarshalCoreProperties(t *testing.T) {
 	}
 
 	testhelper.CompareGoldenXML(t, "core.xml", got.Bytes())
+}
+
+func ExampleCoreProperties() {
+	doc, _ := document.Open("document.docx")
+	cp := doc.CoreProperties
+	// Reading Properties
+	fmt.Println("Title:", cp.Title())
+	fmt.Println("Author:", cp.Author())
+	fmt.Println("Description:", cp.Description())
+	fmt.Println("Last Modified By:", cp.LastModifiedBy())
+	fmt.Println("Category:", cp.Category())
+	fmt.Println("Content Status:", cp.ContentStatus())
+	fmt.Println("Created:", cp.Created())
+	fmt.Println("Modified:", cp.Modified())
+
+	// Setting Properties
+	cp.SetTitle("CP Invoices")
+	cp.SetAuthor("John Doe")
+	cp.SetCategory("Invoices")
+	cp.SetContentStatus("Draft")
+	cp.SetLastModifiedBy("Jane Smith")
+	cp.SetCreated(time.Now())
+	cp.SetModified(time.Now())
+	doc.SaveToFile("document.docx")
 }
