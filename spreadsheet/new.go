f07c57cae76956d8f86bc0fea256b7f1feb20e64
Todd <todd@baliance.com>
2017-09-03 11:05:27 -0500

    gooxml: move content types to base package

--- spreadsheet/new.go
+++ spreadsheet/new.go
@@ -8,6 +8,7 @@
 package spreadsheet
 
 import (
+	"baliance.com/gooxml"
 	"baliance.com/gooxml/common"
 	sml "baliance.com/gooxml/schema/schemas.openxmlformats.org/spreadsheetml"
 	"baliance.com/gooxml/zippkg"
@@ -24,18 +25,18 @@ func New() *Workbook {
 
 	wb.Rels = common.NewRelationships()
 	wb.wbRels = common.NewRelationships()
-	wb.Rels.AddRelationship(zippkg.AppPropsFilename, common.ExtendedPropertiesType)
-	wb.Rels.AddRelationship(zippkg.CorePropsFilename, common.CorePropertiesType)
-	wb.Rels.AddRelationship("xl/workbook.xml", common.OfficeDocumentType)
-	wb.wbRels.AddRelationship("styles.xml", common.StylesType)
+	wb.Rels.AddRelationship(zippkg.AppPropsFilename, gooxml.ExtendedPropertiesType)
+	wb.Rels.AddRelationship(zippkg.CorePropsFilename, gooxml.CorePropertiesType)
+	wb.Rels.AddRelationship("xl/workbook.xml", gooxml.OfficeDocumentType)
+	wb.wbRels.AddRelationship("styles.xml", gooxml.StylesType)
 
 	wb.ContentTypes = common.NewContentTypes()
 	wb.ContentTypes.AddOverride("/xl/workbook.xml", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml")
 	wb.ContentTypes.AddOverride("/xl/styles.xml", "application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml")
 
 	wb.SharedStrings = NewSharedStrings()
-	wb.ContentTypes.AddOverride("/xl/sharedStrings.xml", common.SharedStringsContentType)
-	wb.wbRels.AddRelationship("sharedStrings.xml", common.SharedStingsType)
+	wb.ContentTypes.AddOverride("/xl/sharedStrings.xml", gooxml.SharedStringsContentType)
+	wb.wbRels.AddRelationship("sharedStrings.xml", gooxml.SharedStingsType)
 
 	return wb
 }
