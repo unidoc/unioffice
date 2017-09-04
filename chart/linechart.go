202ff1a67680308776752b27c7940ed2607c25a4
Todd <todd@baliance.com>
2017-09-04 16:01:26 -0500

    spreadsheet: add bar chart support

--- chart/linechart.go
+++ chart/linechart.go
@@ -8,11 +8,11 @@
 package chart
 
 import (
-	"baliance.com/gooxml/color"
 	crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
 )
 
 type LineChart struct {
+	chartBase
 	x *crt.CT_LineChart
 }
 
@@ -21,20 +21,9 @@ func (c LineChart) X() *crt.CT_LineChart {
 	return c.x
 }
 
-var colors = []color.Color{color.RGB(0x33, 0x66, 0xcc), color.RGB(0xDC, 0x39, 0x12), color.RGB(0xFF, 0x99, 0x00),
-	color.RGB(0x10, 0x96, 0x18), color.RGB(0x99, 0x00, 0x99), color.RGB(0x3B, 0x3E, 0xAC), color.RGB(0x00, 0x99, 0xC6),
-	color.RGB(0xDD, 0x44, 0x77), color.RGB(0x66, 0xAA, 0x00), color.RGB(0xB8, 0x2E, 0x2E), color.RGB(0x31, 0x63, 0x95),
-	color.RGB(0x99, 0x44, 0x99), color.RGB(0x22, 0xAA, 0x99), color.RGB(0xAA, 0xAA, 0x11), color.RGB(0x66, 0x33, 0xCC),
-	color.RGB(0xE6, 0x73, 0x00), color.RGB(0x8B, 0x07, 0x07), color.RGB(0x32, 0x92, 0x62), color.RGB(0x55, 0x74, 0xA6),
-	color.RGB(0x3B, 0x3E, 0xAC)}
-
-func (c LineChart) nextColor() color.Color {
-	idx := len(c.x.Ser)
-	return colors[idx%len(colors)]
-}
-
+// AddSeries adds a default series to a line chart.
 func (c LineChart) AddSeries() LineChartSeries {
-	color := c.nextColor()
+	color := c.nextColor(len(c.x.Ser))
 	ser := crt.NewCT_LineSer()
 	c.x.Ser = append(c.x.Ser, ser)
 	ser.Idx.ValAttr = uint32(len(c.x.Ser) - 1)
@@ -46,6 +35,7 @@ func (c LineChart) AddSeries() LineChartSeries {
 	return ls
 }
 
+// AddAxis adds an axis to a line chart.
 func (c LineChart) AddAxis(axis Axis) {
 	axisID := crt.NewCT_UnsignedInt()
 	axisID.ValAttr = axis.AxisID()
