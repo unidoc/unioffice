297336008ad87222d377f2580427133b3aceb2f2
Todd <todd@baliance.com>
2017-09-05 20:53:02 -0400

    spreadsheet: add a no cell data example

--- chart/numberdatasource.go
+++ chart/numberdatasource.go
@@ -7,7 +7,11 @@
 
 package chart
 
-import crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+import (
+	"fmt"
+
+	crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+)
 
 type NumberDataSource struct {
 	x *crt.CT_NumDataSource
@@ -21,4 +25,19 @@ func (n NumberDataSource) SetReference(s string) {
 	n.x.Choice = crt.NewCT_NumDataSourceChoice()
 	n.x.Choice.NumRef = crt.NewCT_NumRef()
 	n.x.Choice.NumRef.F = s
+}
+
+func (n NumberDataSource) SetValues(v []float64) {
+	n.x.Choice = crt.NewCT_NumDataSourceChoice()
+	n.x.Choice.NumLit = crt.NewCT_NumData()
+	n.x.Choice.NumLit.PtCount = crt.NewCT_UnsignedInt()
+	n.x.Choice.NumLit.PtCount.ValAttr = uint32(len(v))
+
+	for i, x := range v {
+		n.x.Choice.NumLit.Pt = append(n.x.Choice.NumLit.Pt,
+			&crt.CT_NumVal{
+				IdxAttr: uint32(i),
+				V:       fmt.Sprintf("%g", x)})
+	}
+
 }
