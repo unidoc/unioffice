38db3f5311e0627f7c274fd3abb71c336c4006c7
Todd <todd@baliance.com>
2017-09-06 17:24:30 -0400

    spreadsheet: add radar chart support

---
+++ chart/radarchart.go
@@ -0,0 +1,46 @@
+// Copyright 2017 Baliance. All rights reserved.
+//
+// Use of this source code is governed by the terms of the Affero GNU General
+// Public License version 3.0 as published by the Free Software Foundation and
+// appearing in the file LICENSE included in the packaging of this file. A
+// commercial license can be purchased by contacting sales@baliance.com.
+
+package chart
+
+import crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+
+// RadarChart is an Radar chart that has a shaded Radar underneath a curve.
+type RadarChart struct {
+	chartBase
+	x *crt.CT_RadarChart
+}
+
+// X returns the inner wrapped XML type.
+func (c RadarChart) X() *crt.CT_RadarChart {
+	return c.x
+}
+
+// InitializeDefaults the bar chart to its defaults
+func (c RadarChart) InitializeDefaults() {
+	c.x.RadarStyle.ValAttr = crt.ST_RadarStyleMarker
+}
+
+// AddSeries adds a default series to an Radar chart.
+func (c RadarChart) AddSeries() RadarChartSeries {
+	clr := c.nextColor(len(c.x.Ser))
+	ser := crt.NewCT_RadarSer()
+	c.x.Ser = append(c.x.Ser, ser)
+	ser.Idx.ValAttr = uint32(len(c.x.Ser) - 1)
+	ser.Order.ValAttr = uint32(len(c.x.Ser) - 1)
+
+	bs := RadarChartSeries{ser}
+	bs.InitializeDefaults()
+	bs.Properties().SetSolidFill(clr)
+	return bs
+}
+
+func (c RadarChart) AddAxis(axis Axis) {
+	axisID := crt.NewCT_UnsignedInt()
+	axisID.ValAttr = axis.AxisID()
+	c.x.AxId = append(c.x.AxId, axisID)
+}
---
+++ chart/radarchartseries.go
@@ -0,0 +1,59 @@
+// Copyright 2017 Baliance. All rights reserved.
+//
+// Use of this source code is governed by the terms of the Affero GNU General
+// Public License version 3.0 as published by the Free Software Foundation and
+// appearing in the file LICENSE included in the packaging of this file. A
+// commercial license can be purchased by contacting sales@baliance.com.
+
+package chart
+
+import (
+	"baliance.com/gooxml/drawing"
+	dml "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml"
+	crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+)
+
+// RadarChartSeries is a series to be used on an Radar chart.
+type RadarChartSeries struct {
+	x *crt.CT_RadarSer
+}
+
+// X returns the inner wrapped XML type.
+func (c RadarChartSeries) X() *crt.CT_RadarSer {
+	return c.x
+}
+
+// InitializeDefaults initializes an Radar series to the default values.
+func (c RadarChartSeries) InitializeDefaults() {
+
+}
+
+// SetText sets the series text.
+func (c RadarChartSeries) SetText(s string) {
+	c.x.Tx = crt.NewCT_SerTx()
+	c.x.Tx.Choice.V = &s
+}
+
+// CategoryAxis returns the category data source.
+func (c RadarChartSeries) CategoryAxis() CategoryAxisDataSource {
+	if c.x.Cat == nil {
+		c.x.Cat = crt.NewCT_AxDataSource()
+	}
+	return MakeAxisDataSource(c.x.Cat)
+}
+
+// Values returns the value data source.
+func (c RadarChartSeries) Values() NumberDataSource {
+	if c.x.Val == nil {
+		c.x.Val = crt.NewCT_NumDataSource()
+	}
+	return MakeNumberDataSource(c.x.Val)
+}
+
+// Properties returns the bar chart series shape properties.
+func (c RadarChartSeries) Properties() drawing.ShapeProperties {
+	if c.x.SpPr == nil {
+		c.x.SpPr = dml.NewCT_ShapeProperties()
+	}
+	return drawing.MakeShapeProperties(c.x.SpPr)
+}
