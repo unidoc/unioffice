04f24736db291530d21f31127f0e30cf3b42ad66
Todd <todd@baliance.com>
2017-09-05 20:53:02 -0400

    spreadsheet: add a no cell data example

--- chart/axisdatasource.go
+++ chart/axisdatasource.go
@@ -7,12 +7,17 @@
 
 package chart
 
-import crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+import (
+	crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+)
 
+// AxisDataSource specifies the data for an axis.  It's commonly used with
+// SetReference to set the axis data to a range of cells.
 type AxisDataSource struct {
 	x *crt.CT_AxDataSource
 }
 
+// MakeAxisDataSource constructs an AxisDataSource wrapper.
 func MakeAxisDataSource(x *crt.CT_AxDataSource) AxisDataSource {
 	return AxisDataSource{x}
 }
@@ -21,4 +26,19 @@ func (a AxisDataSource) SetReference(s string) {
 	a.x.Choice = crt.NewCT_AxDataSourceChoice()
 	a.x.Choice.StrRef = crt.NewCT_StrRef()
 	a.x.Choice.StrRef.F = s
+}
+
+func (a AxisDataSource) SetValues(v []string) {
+	a.x.Choice = crt.NewCT_AxDataSourceChoice()
+	a.x.Choice.StrLit = crt.NewCT_StrData()
+	a.x.Choice.StrLit.PtCount = crt.NewCT_UnsignedInt()
+	a.x.Choice.StrLit.PtCount.ValAttr = uint32(len(v))
+
+	for i, x := range v {
+		a.x.Choice.StrLit.Pt = append(a.x.Choice.StrLit.Pt,
+			&crt.CT_StrVal{
+				IdxAttr: uint32(i),
+				V:       x})
+	}
+
 }
