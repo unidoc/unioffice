b26dccb39a753fe4e9e0ba287e5fa11a02bba730
Todd <todd@baliance.com>
2017-09-07 07:39:56 -0400

    meta: add spreadsheet example

--- example_test.go
+++ example_test.go
@@ -1,6 +1,9 @@
 package gooxml_test
 
-import "baliance.com/gooxml/document"
+import (
+	"baliance.com/gooxml/document"
+	"baliance.com/gooxml/spreadsheet"
+)
 
 func Example_document() {
 	// see the baliance.com/gooxml/document documentation or _examples/document
@@ -8,4 +11,14 @@ func Example_document() {
 	doc := document.New()
 	doc.AddParagraph().AddRun().AddText("Hello World!")
 	doc.SaveToFile("document.docx")
+}
+
+func Example_spreadsheeet() {
+	// see the baliance.com/gooxml/spreadsheet documentation or _examples/spreadsheet
+	// for more examples
+	ss := spreadsheet.New()
+	sheet := ss.AddSheet()
+	sheet.AddRow().AddCell().SetString("Hello")
+	sheet.Cell("B1").SetString("World!")
+	ss.SaveToFile("workbook.xlsx")
 }
