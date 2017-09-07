a93aa51b688d1fb880b629ac808034a7aecdf50b
Todd <todd@baliance.com>
2017-09-07 10:54:54 -0400

    spreadsheet: add support for bubble chart

---
+++ chart/bubblechart.go
@@ -0,0 +1,48 @@
+// Copyright 2017 Baliance. All rights reserved.
+//
+// Use of this source code is governed by the terms of the Affero GNU General
+// Public License version 3.0 as published by the Free Software Foundation and
+// appearing in the file LICENSE included in the packaging of this file. A
+// commercial license can be purchased by contacting sales@baliance.com.
+
+package chart
+
+import (
+	crt "baliance.com/gooxml/schema/schemas.openxmlformats.org/drawingml/2006/chart"
+)
+
+// BubbleChart is a 2D Bubble chart.
+type BubbleChart struct {
+	chartBase
+	x *crt.CT_BubbleChart
+}
+
+// X returns the inner wrapped XML type.
+func (c BubbleChart) X() *crt.CT_BubbleChart {
+	return c.x
+}
+
+// InitializeDefaults the Bubble chart to its defaults
+func (c BubbleChart) InitializeDefaults() {
+
+}
+
+// AddSeries adds a default series to a Bubble chart.
+func (c BubbleChart) AddSeries() BubbleChartSeries {
+	clr := c.nextColor(len(c.x.Ser))
+	ser := crt.NewCT_BubbleSer()
+	c.x.Ser = append(c.x.Ser, ser)
+	ser.Idx.ValAttr = uint32(len(c.x.Ser) - 1)
+	ser.Order.ValAttr = uint32(len(c.x.Ser) - 1)
+
+	bs := BubbleChartSeries{ser}
+	bs.InitializeDefaults()
+	bs.Properties().SetSolidFill(clr)
+	return bs
+}
+
+func (c BubbleChart) AddAxis(axis Axis) {
+	axisID := crt.NewCT_UnsignedInt()
+	axisID.ValAttr = axis.AxisID()
+	c.x.AxId = append(c.x.AxId, axisID)
+}
