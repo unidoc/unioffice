770b2951612f38bad03eb66cc28616991a490ea7
Todd <todd@baliance.com>
2017-09-07 06:40:55 -0400

    meta: speed up example building

--- build-examples.sh
+++ build-examples.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find _examples/ -maxdepth 2 -mindepth 2 -exec sh -c "cd {}; echo building {}; go run main.go" \;
+find _examples/ -maxdepth 2 -mindepth 2 -exec sh -c "cd {}; echo building {}; go build -i main.go" \;
