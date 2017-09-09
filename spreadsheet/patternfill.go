cdb3f64c4a84fd3d94600b1ca6a0792356ca7e0a
Todd <todd@baliance.com>
2017-09-09 10:11:09 -0500

    spreadsheet: more fill cleanup

--- spreadsheet/patternfill.go
+++ spreadsheet/patternfill.go
@@ -13,32 +13,6 @@ import (
 	sml "baliance.com/gooxml/schema/schemas.openxmlformats.org/spreadsheetml"
 )
 
-type Fill struct {
-	x     *sml.CT_Fill
-	fills *sml.CT_Fills
-}
-
-func (f Fill) Index() uint32 {
-	// in differential formats, CT_Fill is not held in a CT_Fills and index
-	// doesn't mean anything
-	if f.fills == nil {
-		return 0
-
-	}
-	for i, sf := range f.fills.Fill {
-		if f.x == sf {
-			return uint32(i)
-		}
-	}
-	return 0
-}
-
-func (f Fill) SetPatternFill() PatternFill {
-	f.x.GradientFill = nil
-	f.x.PatternFill = sml.NewCT_PatternFill()
-	return PatternFill{f.x.PatternFill, f.x}
-}
-
 type PatternFill struct {
 	x *sml.CT_PatternFill
 	f *sml.CT_Fill
