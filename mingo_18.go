f31bbbab2c032e1b4756efa19d7974658dd5fdd6
Todd <todd@baliance.com>
2017-09-21 17:17:45 -0500

    meta: add compile time check for go1.8+

---
+++ mingo_18.go
@@ -0,0 +1,12 @@
+// Copyright 2017 Baliance. All rights reserved.
+//
+// Use of this source code is governed by the terms of the Affero GNU General
+// Public License version 3.0 as published by the Free Software Foundation and
+// appearing in the file LICENSE included in the packaging of this file. A
+// commercial license can be purchased by contacting sales@baliance.com.
+
+// +build go1.8
+
+package gooxml
+
+const requires_go_18 = true
