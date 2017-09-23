f2b870cb37425bc6646ef9f1840d7275375d0e31
Todd <todd@baliance.com>
2017-09-22 20:42:46 -0500

    chart: add some tests
    The tests aren't much, but they do execute
    some functionality that should always work.

--- chart/chart_test.go
+++ chart/chart_test.go
@@ -49,3 +49,300 @@ func TestTitle(t *testing.T) {
 		t.Errorf("after remove, title deleted should be true")
 	}
 }
+func TestDateAxis(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+
+	c.AddDateAxis()
+	c.AddCategoryAxis()
+	c.AddLineChart()
+}
+func TestArea3DChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddArea3DChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Area3D chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+	s.SetText("foo")
+	if *s.X().Tx.Choice.V != "foo" {
+		t.Errorf("expected foo")
+	}
+
+	s.CategoryAxis()
+	s.Values()
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestAreaChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddAreaChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Area chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+	s.SetText("foo")
+	if *s.X().Tx.Choice.V != "foo" {
+		t.Errorf("expected foo")
+	}
+
+	s.CategoryAxis()
+	s.Values()
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestBar3DChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddBar3DChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Bar3D chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	s.SetText("foo")
+	if *s.X().Tx.Choice.V != "foo" {
+		t.Errorf("expected foo")
+	}
+
+	s.CategoryAxis()
+	s.Values()
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestBarChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddBarChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Bar chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestBubbleChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddBubbleChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Bubble chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	s.SetText("foo")
+	if *s.X().Tx.Choice.V != "foo" {
+		t.Errorf("expected foo")
+	}
+
+	s.CategoryAxis()
+	s.Values()
+	s.BubbleSizes()
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestDoughnutChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddDoughnutChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Doughnut chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+}
+
+func TestLine3DChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddLine3DChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Line3D chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestLineChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddLineChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Line chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestPieChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddPieChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Pie chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+}
+func TestPie3DChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddPie3DChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Pie3D chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+}
+
+func TestStockChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddStockChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Stock chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestSurfaceChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddSurfaceChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Surface chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+func TestSurface3DChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddSurface3DChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Surface3D chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestRadarChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddRadarChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Radar chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
+
+func TestPieOfPieChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddPieOfPieChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil PieOfPie chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+}
+
+func TestScatterChart(t *testing.T) {
+	spc := crt.NewChartSpace()
+	c := chart.MakeChart(spc)
+	crt := c.AddScatterChart()
+	if crt.X() == nil {
+		t.Errorf("expected non-nil Scatter chart")
+	}
+
+	s := crt.AddSeries()
+	if s.X() == nil {
+		t.Errorf("expected non-nil series")
+	}
+
+	va := c.AddValueAxis()
+	crt.AddAxis(va)
+}
