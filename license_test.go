665d3a281d2e6cb2f6c3005982ff99c65a4c6fba
Todd <todd@baliance.com>
2017-09-29 19:06:16 -0500

    gooxml: add support for verifying versions and license

---
+++ license_test.go
@@ -0,0 +1,15 @@
+// Copyright 2017 Baliance. All rights reserved.
+//
+// Use of this source code is governed by the terms of the Affero GNU General
+// Public License version 3.0 as published by the Free Software Foundation and
+// appearing in the file LICENSE included in the packaging of this file. A
+// commercial license can be purchased by contacting sales@baliance.com.
+
+package gooxml_test
+
+import "testing"
+import "baliance.com/gooxml"
+
+func TestOpenSourceLicense(t *testing.T) {
+	gooxml.GetLicense()
+}
