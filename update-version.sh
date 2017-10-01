c051caab852d894cf1627010f1d14e86a3afd586
Todd <todd@baliance.com>
2017-10-01 11:34:37 -0500

    gooxml: add release info to metadata in generated files

--- update-version.sh
+++ update-version.sh
@@ -13,6 +13,9 @@ package gooxml
 
 import "time"
 
+// Release is the last release version of the software.
+var ReleaseVersion = "v0.4.0"
+
 // ReleaseDate is the release date of the source code for licensing purposes.
 var ReleaseDate = time.Date(`date +%Y`,`date +%_m`,`date +%_d`,0,0,0,0,time.UTC)
 
--- version.go
+++ version.go
@@ -9,5 +9,8 @@ package gooxml
 
 import "time"
 
+// Release is the last release version of the software.
+var ReleaseVersion = "v0.4.0"
+
 // ReleaseDate is the release date of the source code for licensing purposes.
-var ReleaseDate = time.Date(2017, 9, 30, 0, 0, 0, 0, time.UTC)
+var ReleaseDate = time.Date(2017, 10, 1, 0, 0, 0, 0, time.UTC)
