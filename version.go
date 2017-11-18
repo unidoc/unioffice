8b1da53e6d47706e302a73924157b78a1e08281a
Todd <todd@baliance.com>
2017-11-18 11:55:06 -0600

    meta: update version information

--- version.go
+++ version.go
@@ -13,4 +13,4 @@ import "time"
 var ReleaseVersion = "v0.4.0"
 
 // ReleaseDate is the release date of the source code for licensing purposes.
-var ReleaseDate = time.Date(2017, 10, 1, 0, 0, 0, 0, time.UTC)
+var ReleaseDate = time.Date(2017, 11, 18, 0, 0, 0, 0, time.UTC)
