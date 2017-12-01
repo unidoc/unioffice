0482706b6cf0d254111a2e636acee04b949ee264
Todd <todd@baliance.com>
2017-12-01 12:13:19 -0600

    document: add support for creating bookmarks
    - support adding bookmarks
    - support hyperlinks that reference a bookmark
    - update hyperlink example
    
    Fixes #126

---
+++ document/bookmark.go
@@ -0,0 +1,31 @@
+// Copyright 2017 Baliance. All rights reserved.
+//
+// Use of this source code is governed by the terms of the Affero GNU General
+// Public License version 3.0 as published by the Free Software Foundation and
+// appearing in the file LICENSE included in the packaging of this file. A
+// commercial license can be purchased by contacting sales@baliance.com.
+
+package document
+
+import "baliance.com/gooxml/schema/soo/wml"
+
+type Bookmark struct {
+	x *wml.CT_Bookmark
+}
+
+// X returns the inner wrapped XML type.
+func (b Bookmark) X() *wml.CT_Bookmark {
+	return b.x
+}
+
+// SetName sets the name of the bookmark. This is the name that is used to
+// reference the bookmark from hyperlinks.
+func (b Bookmark) SetName(name string) {
+	b.x.NameAttr = name
+}
+
+// Name returns the name of the bookmark whcih is the document unique ID that
+// identifies the bookmark.
+func (b Bookmark) Name() string {
+	return b.x.NameAttr
+}
