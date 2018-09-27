a98577eda743010db3fd58209419eb871ac47f20
Todd <todd@baliance.com>
2018-09-27 11:37:48 -0500

    spreadsheet: support inserting rows within a sheet
    Fixes #203

---
+++ _examples/spreadsheet/insert-rows/main.go
@@ -0,0 +1,36 @@
+// Copyright 2017 Baliance. All rights reserved.
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"baliance.com/gooxml/spreadsheet"
+)
+
+func main() {
+	ss := spreadsheet.New()
+	// add a single sheet
+	sheet := ss.AddSheet()
+
+	// rows
+	for r := 0; r < 5; r++ {
+		row := sheet.AddRow()
+		// and cells
+		for c := 0; c < 5; c++ {
+			cell := row.AddCell()
+			cell.SetString(fmt.Sprintf("row %d cell %d", r, c))
+		}
+	}
+
+	// no insert some rows after row 2
+	for i := 0; i < 4; i++ {
+		sheet.InsertRow(2).AddCell().SetString(fmt.Sprintf("inserted at 2, iter %d", i))
+	}
+
+	if err := ss.Validate(); err != nil {
+		log.Fatalf("error validating sheet: %s", err)
+	}
+
+	ss.SaveToFile("insert-rows.xlsx")
+}
