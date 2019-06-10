9dcbb5004754eaae9792d1f0d8d826a9c77e4d01
Marc Coury <gloriphobia@gmail.com>
2019-06-10 12:52:22 +0100

    fix adding images from bytes for presentation

--- presentation/presentation.go
+++ presentation/presentation.go
@@ -446,6 +446,10 @@ func (p *Presentation) Save(w io.Writer) error {
 			if err := zippkg.AddFileFromDisk(z, fn, img.Path()); err != nil {
 				return err
 			}
+		} else if img.Data() != nil && len(*img.Data()) > 0 {
+			if err := zippkg.AddFileFromBytes(z, fn, *img.Data()); err != nil {
+				return err
+			}
 		} else {
 			unioffice.Log("unsupported image source: %+v", img)
 		}
