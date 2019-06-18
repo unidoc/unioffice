837139941d8485d17135d7ac40fb1450de3272dd
Marc Coury <gloriphobia@gmail.com>
2019-06-18 11:41:20 +0100

    add AbsoluteImageFilename

--- common/doc.go
+++ common/doc.go
@@ -12,14 +12,13 @@ import (
 
 //AddImageToZip adds an image (either from bytes or from disk) and adds it to the zip file.
 func AddImageToZip(z *zip.Writer, img ImageRef, imageNum int, dt unioffice.DocType) error {
-	fn := unioffice.AbsoluteFilename(dt, unioffice.ImageType, imageNum)
-	fn = fn[0:len(fn)-3] + strings.ToLower(img.Format())
+	filename := unioffice.AbsoluteImageFilename(dt, imageNum, strings.ToLower(img.Format()))
 	if img.Data() != nil && len(*img.Data()) > 0 {
-		if err := zippkg.AddFileFromBytes(z, fn, *img.Data()); err != nil {
+		if err := zippkg.AddFileFromBytes(z, filename, *img.Data()); err != nil {
 			return err
 		}
 	} else if img.Path() != "" {
-		if err := zippkg.AddFileFromDisk(z, fn, img.Path()); err != nil {
+		if err := zippkg.AddFileFromDisk(z, filename, img.Path()); err != nil {
 			return err
 		}
 	} else {
