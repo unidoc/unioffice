bb03a3a2b03d08e1d386312b5cbef9cc4fef199d
Marc Coury <gloriphobia@gmail.com>
2019-06-18 10:57:56 +0100

    make adding images consistent across doc types

--- common/doc.go
+++ common/doc.go
@@ -1,3 +1,29 @@
 // Package common contains wrapper types and utilities common to all of the
 // OOXML document formats.
 package common
+
+import (
+	"archive/zip"
+	"strings"
+
+	"github.com/mec07/unioffice/zippkg"
+	"github.com/unidoc/unioffice"
+)
+
+//AddImageToZip adds an image (either from bytes or from disk) and adds it to the zip file.
+func AddImageToZip(z *zip.Writer, img ImageRef, i int, dt unioffice.DocType) error {
+	fn := unioffice.AbsoluteFilename(dt, unioffice.ImageType, i+1)
+	fn = fn[0:len(fn)-3] + strings.ToLower(img.Format())
+	if img.Data() != nil && len(*img.Data()) > 0 {
+		if err := zippkg.AddFileFromBytes(z, fn, *img.Data()); err != nil {
+			return err
+		}
+	} else if img.Path() != "" {
+		if err := zippkg.AddFileFromDisk(z, fn, img.Path()); err != nil {
+			return err
+		}
+	} else {
+		unioffice.Log("unsupported image source: %+v", img)
+	}
+	return nil
+}
--- presentation/presentation.go
+++ presentation/presentation.go
@@ -19,7 +19,6 @@ import (
 	"log"
 	"os"
 	"path"
-	"strings"
 
 	"github.com/unidoc/unioffice"
 	"github.com/unidoc/unioffice/common"
@@ -440,18 +439,8 @@ func (p *Presentation) Save(w io.Writer) error {
 	}
 
 	for i, img := range p.Images {
-		fn := unioffice.AbsoluteFilename(unioffice.DocTypePresentation, unioffice.ImageType, i+1)
-		fn = fn[0:len(fn)-3] + strings.ToLower(img.Format())
-		if img.Path() != "" {
-			if err := zippkg.AddFileFromDisk(z, fn, img.Path()); err != nil {
-				return err
-			}
-		} else if img.Data() != nil && len(*img.Data()) > 0 {
-			if err := zippkg.AddFileFromBytes(z, fn, *img.Data()); err != nil {
-				return err
-			}
-		} else {
-			unioffice.Log("unsupported image source: %+v", img)
+		if err := common.AddImageToZip(z, img, i, unioffice.DocTypePresentation); err != nil {
+			return err
 		}
 	}
 
