d351975af1e36675a9ad2b7adbbeb45908e6922d
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2019-07-25 19:43:46 +0300

    Baliance replaced for FoxyUtils ehf in License information (#312)

--- color/color.go
+++ color/color.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Baliance. All rights reserved.
+// Copyright 2017 FoxyUtils ehf. All rights reserved.
 //
 // Use of this source code is governed by the terms of the Affero GNU General
 // Public License version 3.0 as published by the Free Software Foundation and
