eb78db5121f4f3165b237377a8ef6ff8c10582cc
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2019-08-02 13:30:39 +0300

    Licanse info fix

--- schema/soo/pkg/content_types/Override_test.go
+++ schema/soo/pkg/content_types/Override_test.go
@@ -5,7 +5,7 @@
 // Use of this source code is governed by the terms of the Affero GNU General
 // Public License version 3.0 as published by the Free Software Foundation and
 // appearing in the file LICENSE included in the packaging of this file. A
-// commercial license can be purchased by contacting sales@baliance.com.
+// commercial license can be purchased via https://unidoc.io website.
 
 package content_types_test
 
