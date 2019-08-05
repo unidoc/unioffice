541b6a88da14d35cae51d04c787e941ffe78a059
Sandro Trianni <sandro.trianni@gmail.com>
2019-08-05 21:14:04 +0200

    Refactor header/footer reusing document.tables (#308)
    * Refactor header.go  Reuse document.tables
    * Refactor footer.go  Reuse document.tables

--- document/footer.go
+++ document/footer.go
@@ -55,7 +55,7 @@ func (f Footer) Tables() []Table {
 		return nil
 	}
 	for _, c := range f.x.EG_ContentBlockContent {
-		for _, t := range f.tables(f.d, c) {
+		for _, t := range f.d.tables(c) {
 			ret = append(ret, t)
 		}
 	}
@@ -127,28 +127,4 @@ func (f Footer) AddImage(i common.Image) (common.ImageRef, error) {
 	rel := ftrRels.AddRelationship(fn, unioffice.ImageType)
 	r.SetRelID(rel.X().IdAttr)
 	return r, nil
-}
-
-func (f Footer) tables(d *Document, bc *wml.EG_ContentBlockContent) []Table {
-	ret := []Table{}
-	for _, t := range bc.Tbl {
-		ret = append(ret, Table{d, t})
-		for _, crc := range t.EG_ContentRowContent {
-			for _, tr := range crc.Tr {
-				for _, ccc := range tr.EG_ContentCellContent {
-					for _, tc := range ccc.Tc {
-						for _, ble := range tc.EG_BlockLevelElts {
-							for _, cbc := range ble.EG_ContentBlockContent {
-								for _, tbl := range f.tables(d, cbc) {
-									ret = append(ret, tbl)
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return ret
 }
