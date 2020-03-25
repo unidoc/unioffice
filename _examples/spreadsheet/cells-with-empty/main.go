d32b67abf5d10684c6b651d259a96a0f6148aabd
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2020-03-25 11:52:16 +0300

    goimports example file

--- _examples/spreadsheet/cells-with-empty/main.go
+++ _examples/spreadsheet/cells-with-empty/main.go
@@ -1,10 +1,11 @@
 // Copyright 2017 FoxyUtils ehf. All rights reserved.
 package main
+
 // This example demonstrates outputing all cells in a row of an excel spreadsheet, including empty cells.
 
 import (
-	"log"
 	"fmt"
+	"log"
 
 	"github.com/unidoc/unioffice/spreadsheet"
 )
@@ -18,18 +19,18 @@ func main() {
 	s := ss.Sheets()[0]
 
 	maxColumnIdx := s.MaxColumnIdx()
-	for _, row := range s.Rows(){
-		for _,cell := range row.CellsWithEmpty(maxColumnIdx){
-			fmt.Println(cell.Reference(),":", cell.GetFormattedValue())
+	for _, row := range s.Rows() {
+		for _, cell := range row.CellsWithEmpty(maxColumnIdx) {
+			fmt.Println(cell.Reference(), ":", cell.GetFormattedValue())
 		}
 	}
 	fmt.Print("\n\n\n")
 
 	s.Cell("F4").SetString("Hello world")
 	maxColumnIdx = s.MaxColumnIdx()
-	for _, row := range s.Rows(){
-		for _,cell := range row.CellsWithEmpty(maxColumnIdx){
-			fmt.Println(cell.Reference(),":", cell.GetFormattedValue())
+	for _, row := range s.Rows() {
+		for _, cell := range row.CellsWithEmpty(maxColumnIdx) {
+			fmt.Println(cell.Reference(), ":", cell.GetFormattedValue())
 		}
 	}
 }
