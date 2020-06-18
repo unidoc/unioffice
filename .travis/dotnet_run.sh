8e0341ce856fb11a4f3b3ef4ff325ac38adf5f97
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2020-06-18 13:04:08 +0000

    only errors are printed in travis log

--- .travis/dotnet_run.sh
+++ .travis/dotnet_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 output=$(dotnet ./openxml-validator/bin/Debug/netcoreapp3.0/Program.dll --$2 $1)
-echo $output
-echo ""
 if [[ $output == *"is not valid"* ]]; then
-	if [[ $output != *"main:sz"* ]] && [[ $output != *"main:family"* ]]; then
+	if [[ $output != *"main:sz"* ]] && [[ $output != *"main:family"* ]] && [[ $output != *"Attribute 'si' should be present when the value of attribute 't' is 'shared'"* ]] ; then
+		echo $output
+		echo ""
 		echo $output >> errors
 		echo "" >> errors
 	fi
