7dfc49f1425370fe5f0ff09e36e30ac659565f1e
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2020-06-18 12:52:06 +0000

    travis fix

--- .travis/dotnet_run.sh
+++ .travis/dotnet_run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 output=$(dotnet ./openxml-validator/bin/Debug/netcoreapp3.0/Program.dll --$2 $1)
+echo $output
+echo ""
 if [[ $output == *"is not valid"* ]]; then
 	if [[ $output != *"main:sz"* ]] && [[ $output != *"main:family"* ]]; then
-		>&2 echo $output\\n
+		echo $output >> errors
+		echo "" >> errors
 	fi
 fi
