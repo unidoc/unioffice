7c7438f3f36b276ca8dac039146bef45219caf2b
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2020-06-18 18:34:01 +0300

    build-examples.sh exits with error status in the case of build error

--- build-examples.sh
+++ build-examples.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 go get -u github.com/go-ole/go-ole/oleutil
-find _examples/ -maxdepth 2 -mindepth 2 -exec sh -c "cd {}; echo building {}; go build -i main.go" \;
+:> build_errors
+find _examples/ -maxdepth 2 -mindepth 2 -exec sh -c "cd {}; echo building {}; go build -i main.go" 2>>build_errors \;
+wc -l build_errors | awk '{print $1}'
+if [[ $(wc -l build_errors | awk '{print $1}') == "0" ]]; then
+	exit 0
+fi
+exit 1
