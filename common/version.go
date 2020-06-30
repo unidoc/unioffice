26710672368a02e9e7a8cc8bc2067b540249dff8
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2020-06-30 16:15:25 +0000

    Update version and release date for v1.4.0

--- common/version.go
+++ common/version.go
@@ -10,13 +10,13 @@ import (
 	"time"
 )
 
-const releaseYear = 2019
+const releaseYear = 2020
 const releaseMonth = 6
-const releaseDay = 6
-const releaseHour = 11
-const releaseMin = 30
+const releaseDay = 30
+const releaseHour = 16
+const releaseMin = 10
 
 // Version holds version information, when bumping this make sure to bump the released at stamp also.
-const Version = "1.2.0"
+const Version = "1.4.0"
 
 var ReleasedAt = time.Date(releaseYear, releaseMonth, releaseDay, releaseHour, releaseMin, 0, 0, time.UTC)
