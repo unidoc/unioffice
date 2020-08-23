25f34f3f929930dcc59fd85da90e30f79a6d5ff7
UniDoc Build <build@unidoc.io>
2020-08-23 14:15:53 +0000

    prepare release

--- common/doc.go
+++
@@ -1,3 +0,0 @@
-// Package common contains wrapper types and utilities common to all of the
-// OOXML document formats.
-package common
--- spreadsheet/format/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "lexer"
-ragel -G2 -Z lexer.rl
-goimports -w lexer.go
-
-ragel -G2 -Z isnumber.rl
-goimports -w isnumber.go
-
